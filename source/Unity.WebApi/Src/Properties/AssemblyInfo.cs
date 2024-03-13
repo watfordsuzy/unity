35798a54dbdca29f85874976c10d127c15ae8061
Christopher Watford <christopherw@suzy.com>
2024-03-12 21:39:22 -0400

    Drop net45, build Unity.Mvc and Unity.WebApi

--- source/Unity.WebApi/Src/Properties/AssemblyInfo.cs
+++ source/Unity.WebApi/Src/Properties/AssemblyInfo.cs
@@ -6,24 +6,6 @@ using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: CLSCompliant(true)]
-[assembly: AssemblyTitle("Microsoft Unity Application Block integation for ASP.NET WebApi")]
-[assembly: AssemblyDescription("")]
-
-[assembly: AssemblyProduct("Microsoft Unity Application Block")]
-[assembly: AssemblyCompany("Microsoft Corporation")]
-
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCopyright("Copyright © Microsoft Corporation")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: AssemblyVersion("3.5.0.0")]
-[assembly: AssemblyFileVersion("3.5.1404.0")]
-[assembly: AssemblyInformationalVersion("3.5.1404.0")]
 
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en")]
